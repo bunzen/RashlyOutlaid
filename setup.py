eb7506f1eec53ad375215d59c7e78e0b9f37fd76
geir skjotskift <geir@tux.elf.is>
2014-03-22 11:37:23 +0100

    initial checkin

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "RashlyOutlaid",
+    version = "0.9.0",
+    author = "Geir Skjotskift",
+    author_email = "geir@underworld.no",
+    description = "Perform ASN Whois against shadowserver.org",
+    license = "MIT",
+    keywords = "asn whois shadowserver",
+    url = "https://github.com/bunzen/RashlyOutlaid",
+    packages=['RashlyOutlaid', ],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
