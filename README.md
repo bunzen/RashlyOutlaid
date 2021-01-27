c830f8c20581407f958e78681d0d664496c926fe
Geir Skjotskfit <geir@mnemonic.no>
2021-01-27 14:05:33 +0100

    Shadowserver web api support

--- README.md
+++ README.md
@@ -1,41 +1,84 @@
 RashlyOutlaid
 =============
 
-Perform ASN whois queries against shadowserver.org
+Library to interact with the [shadowserver](https://www.shadowserver.org) API and ASN whois services.
 
 about
 -----
 
- Performs whois queries against asn.shadowserver.org. If you query for a list of IP-addresses the library will perform a properly formated bulk query as described and required by the Shadowserver foundation.
-
-The result is cached. If the peers property or the query property is changed, a new query will be executed when the result property is called. Subsequent calls to .result will only access the cached result.
+ Performs api or whois queries against api.shadowserver.org and
+asn.shadowserver.org.  If you query for a list of IP-addresses the library will
+perform a properly formated bulk query as described and required by the
+Shadowserver foundation.
 
 install
 -------
 
 ```bash
-# pip install RashlyOutlaid
+# python3 -m pip install RashlyOutlaid
 ```
 
 shadowserver service
 --------------------
 
-http://wiki.shadowserver.org/wiki/pmwiki.php/Services/IP-BGP
+- The API is rate limited. At this time of writing (January 2021) the current
+limit is is set to 10 queries per second. Verify the current limits on
+[The Shadowserver API](https://www.shadowserver.org/what-we-do/network-reporting/api-asn-and-network-queries/)
+
+- [The Shadowswerver IP-BGP Service](http://wiki.shadowserver.org/wiki/pmwiki.php/Services/IP-BGP)
 
 Example
 -------
 
 ```python
->>> from RashlyOutlaid.libwhois import ASNWhois
->>> asnwhois = ASNWhois()
->>> asnwhois.query = ["212.58.246.94", "94.229.76.35"]
->>> asnwhois.peers = True
->>> asnwhois.result["212.58.246.94"]
-ASNRecord(asn='2818', prefix='212.58.224.0/19', asname='BBC', cn='GB', isp='BBC Internet Services, UK, GB', peers=['286', '3356'])
->>> for q, r in asnwhois.result.items():
-...    print q, r.cn, r.isp
-...
-94.229.76.35 GB AS UK Dedicated Servers, Hosting and Co-Location, GB
-212.58.246.94 GB BBC Internet Services, UK, GB
+>>> import RashlyOutlaid.api as shadowserver
+>>> from pprint import pprint as pp
+>>> pp(shadowserver.prefix(22414))
+['208.82.236.0/22']
+>>> pp(shadowserver.asn(109))
+[ASNRecord(asn='109', prefix='', asname='CISCOSYSTEMS', cn='US', isp='CISCOSYSTEMS', peers=[])]
+>>> pp(shadowserver.origin(["8.8.8.8", "8.8.4.4", "4.2.2.4"]))
+[ASNRecord(asn='15169', prefix='8.8.8.0/24', asname='GOOGLE', cn='US', isp='GOOGLE', peers=[]),
+ ASNRecord(asn='15169', prefix='8.8.4.0/24', asname='GOOGLE', cn='US', isp='GOOGLE', peers=[]),
+ ASNRecord(asn='3356', prefix='4.0.0.0/9', asname='LEVEL3', cn='US', isp='LEVEL3', peers=[])]
+>>> pp(shadowserver.peer(["8.8.8.8", "8.8.4.4", "4.2.2.4"]))
+[ASNRecord(asn='15169', prefix='8.8.8.0/24', asname='GOOGLE', cn='US', isp='GOOGLE', peers=['1101', '6696', '47605', '51088']),
+ ASNRecord(asn='15169', prefix='8.8.4.0/24', asname='GOOGLE', cn='US', isp='GOOGLE', peers=['1101', '6696', '47605', '51088']),
+ ASNRecord(asn='3356', prefix='4.0.0.0/9', asname='LEVEL3', cn='US', isp='LEVEL3', peers=['2914', '6453', '6461', '47605'])]
 >>>
+>>> for r in shadowserver.malware(["dfe1832e02888422f48d6896dc8e8f73","d41d8cd98f00b204e9800998ecf8427e"]):
+...    print(f"{r.sha1} First Seen: {r.first_seen.year}")
+...    for av in r.anti_virus:
+...       print(f"{av.vendor} {av.signature} {av.timestamp.year}")
+...
+c56ba498d41caa7be3c1eb5588cec27c413eb208 First Seen: 2016
+Fortinet W32/Lamer.CQ 2017
+Avast Win32:Lamer-A 2018
+AVG Win32.Generic.VC 2016
+Avast Win32:Malware-gen 2018
+K7GW Virus ( 004d554e1 ) 2016
+MicroWorld Gen:Win32.FileInfector.uwZ@a4T!Kcmi 2017
+Sophos Troj/Agent-APCU 2018
+Eset Win32/Zatoxp.C 2018
+K7 Virus ( 004d554e1 ) 2016
+Avast Win32:Malware-gen 2018
+Avira TR/Dropper.Gen8 2016
+BitDefender Gen:Win32.Backdoor.ozZbauKWKdpb 2018
+DrWeb Win32.HLLW.Siggen.4657 2018
+K7GW Virus ( 004d554e1 ) 2016
+AhnLab Trojan/Win32.FileInfector 2018
+AhnLab Trojan/Win32.FileInfector 2018
+QuickHeal W32.Sivis.A5 2017
+Clam PUA.Win.Packer.Purebasic-2 2017
+BitDefender Gen:Win32.FileInfector.uwZ@a4T!Kcmi 2017
+AVG Win32.Generic.VC 2016
+Ikarus Gen.Win32.FileInfector 2018
+BitDefender Trojan.PWS.Onlinegames.KEGA 2018
+BitDefender Trojan.GenericKD.40542465 2018
+BitDefender Gen:Win32.FileInfector.uwZ@a4T!Kcmi 2017
+Clam PUA.Win.Packer.Purebasic-2 2017
+Sunbelt Virus.Win32.sivis.a 2018
+da39a3ee5e6b4b0d3255bfef95601890afd80709 First Seen: 2015
+>>>
+
 ```
---
+++ RashlyOutlaid/api.py
@@ -0,0 +1,184 @@
+""" Copyright (c) 2014-2021 Geir Skjotskift
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+"""
+
+from collections import namedtuple
+from typing import Dict, List, Text
+import datetime
+
+import requests
+
+import RashlyOutlaid
+
+ASNRecord = namedtuple('ASNRecord',
+                       ['asn', 'prefix', 'asname', 'cn', 'isp', 'peers'])
+AVRecord = namedtuple('AVRecord', ['md5', 'vendor', 'signature', 'timestamp'])
+MalwareRecord = namedtuple('MalwareRecord',
+                           ['timestamp',
+                            'first_seen',
+                            'last_seen',
+                            'type',
+                            'sha256',
+                            'md5',
+                            'sha1',
+                            'pehash',
+                            'tlsh',
+                            'import_hash',
+                            'entropy',
+                            'filesize',
+                            'adobe_malware_classifier',
+                            'magic',
+                            'anti_virus'])
+
+
+def parse_shadowserver_time(time_string: Text) -> datetime.datetime:
+    """Parse a date on the format '2018-10-17 20:36:23'"""
+
+    try:
+        return datetime.datetime.strptime(time_string[:19], '%Y-%m-%d %H:%M:%S')
+    except:
+        print(time_string)
+        raise
+
+
+def malware(hashes: List[Text]) -> List[MalwareRecord]:
+    """Lookup the list of hashes using the Shadowserver malware aPI
+    https://www.shadowserver.org/what-we-do/network-reporting/api-asn-and-network-queries/
+    """
+
+    url = (f'https://api.shadowserver.org/malware/info'
+           f'?sample={",".join(hashes)}')
+
+    res = requests.get(url)
+
+    if res.status_code != 200:
+        msg = (f"RashlyOutlaid.api.origin could not loopup origin. "
+               f"Got status='{res.status_code}' while requesing '{url}'")
+        raise RashlyOutlaid.libwhois.QueryError(msg)
+
+    ss_data: Dict = res.json()
+    return [MalwareRecord(parse_shadowserver_time(elem["timestamp"]),
+                          parse_shadowserver_time(elem["first_seen"]),
+                          parse_shadowserver_time(elem["last_seen"]),
+                          elem.get("type"),
+                          elem.get("sha256"),
+                          elem.get("md5"),
+                          elem.get("sha1"),
+                          elem.get("pehash"),
+                          elem.get("tlsh"),
+                          elem.get("import_hash"),
+                          elem.get("entropic"),
+                          elem.get("filesize"),
+                          elem.get("adobe_malware_classifier"),
+                          elem.get("magic"),
+                          [AVRecord(x.get("md5"),
+                                    x.get("vendor"),
+                                    x.get("signature"),
+                                    parse_shadowserver_time(x["timestamp"]))
+                           for x in elem["anti_virus"]])
+            for elem in ss_data]
+
+
+def _map_shadowserver_model(ssdata: List[Dict]) -> List[ASNRecord]:
+    """Map the result from shadowserver to a list of ASNRecords"""
+
+    return [ASNRecord(x['asn'],
+                      x['prefix'] if 'prefix' in x else '',
+                      x['asname_short'],
+                      x['geo'],
+                      x['asname_long'],
+                      x['peer'].split() if 'peer' in x else [])
+            for x in ssdata]
+
+
+def origin(ip_addresses: List) -> List[ASNRecord]:
+    """Lookup the list of ip addresses vs the Shadowserver origin web api
+    https://www.shadowserver.org/what-we-do/network-reporting/api-asn-and-network-queries/
+    """
+
+    url = (f"https://api.shadowserver.org/net/asn"
+           f"?origin={','.join(ip_addresses)}")
+
+    res = requests.get(url)
+
+    if res.status_code != 200:
+        msg = (f"RashlyOutlaid.api.origin could not loopup origin. "
+               f"Got status='{res.status_code}' while requesing '{url}'")
+        raise RashlyOutlaid.libwhois.QueryError(msg)
+
+    ss_data: List[Dict] = res.json()
+    return _map_shadowserver_model(ss_data)
+
+
+def peer(ip_addresses: List) -> List[ASNRecord]:
+    """Lookup the list of ip addresses vs the Shadowserver peer web api
+    https://www.shadowserver.org/what-we-do/network-reporting/api-asn-and-network-queries/
+    """
+
+    url = (f"https://api.shadowserver.org/net/asn"
+           f"?peer={','.join(ip_addresses)}")
+
+    res = requests.get(url)
+
+    if res.status_code != 200:
+        msg = (f"RashlyOutlaid.api.origin could not loopup origin. "
+               f"Got status='{res.status_code}' while requesing '{url}'")
+        raise RashlyOutlaid.libwhois.QueryError(msg)
+
+    ss_data: List[Dict] = res.json()
+    return _map_shadowserver_model(ss_data)
+
+
+def asn(asnumber: int) -> List[ASNRecord]:
+    """Lookup the asn via the Shadowserver peer web api
+    https://www.shadowserver.org/what-we-do/network-reporting/api-asn-and-network-queries/
+    """
+
+    url = (f"https://api.shadowserver.org/net/asn"
+           f"?query={asnumber}")
+
+    res = requests.get(url)
+
+    if res.status_code != 200:
+        msg = (f"RashlyOutlaid.api.origin could not loopup origin. "
+               f"Got status='{res.status_code}' while requesing '{url}'")
+        raise RashlyOutlaid.libwhois.QueryError(msg)
+
+    ss_data: List[Dict] = [res.json()]
+    return _map_shadowserver_model(ss_data)
+
+
+def prefix(asnumber: int) -> List[Text]:
+    """Lookup the list of ip addresses vs the Shadowserver peer web api
+    https://www.shadowserver.org/what-we-do/network-reporting/api-asn-and-network-queries/
+    """
+
+    url = (f"https://api.shadowserver.org/net/asn"
+           f"?prefix={asnumber}")
+
+    res = requests.get(url)
+
+    if res.status_code != 200:
+        msg = (f"RashlyOutlaid.api.origin could not loopup origin. "
+               f"Got status='{res.status_code}' while requesing '{url}'")
+        raise RashlyOutlaid.libwhois.QueryError(msg)
+
+    ss_data: List[Text] = res.json()
+    return ss_data
